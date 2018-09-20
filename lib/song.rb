8a57cb1dd2db15d21fbe56ea164608666799a290
github username <github email address>
2018-09-19 22:13:14 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,19 @@ class Song
     DB[:conn].execute(sql)
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
+  end
+
 end
-
-
-
